4426b86c6ca9fa1bfcf13dc9264b6ae6169a6256
root <root@22ea66840f1c.mylabserver.com>
2023-06-14 16:29:56 +0000

    fixed warnings

--- webapp.sh
+++ webapp.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# Create project directory
+mkdir webapp
+cd webapp
+
+# Create pom.xml file
+cat <<EOF > pom.xml
 <project xmlns="http://maven.apache.org/POM/4.0.0"
          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
          xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
@@ -8,9 +16,23 @@
     <version>1.0.0</version>
 
     <properties>
-        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
-        <project.reporting.outputEncoding>UTF-8</project.reporting.outputEncoding>
         <maven.compiler.source>1.8</maven.compiler.source>
         <maven.compiler.target>1.8</maven.compiler.target>
     </properties>
 </project>
+EOF
+
+# Create source directory
+mkdir -p src/main/java/com/example
+
+# Create HelloWorld.java file
+cat <<EOF > src/main/java/com/example/HelloWorld.java
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven!");
+    }
+}
+EOF
+
