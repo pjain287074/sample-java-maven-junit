783fdbf6ab36af7f637dc770bf0f69d8d182842d
Pankaj Jain <pankaj_jain@epam.com>
2023-07-20 13:26:34 +0530

    dsl jobs

---
+++ dsl_jobs/job1.groovy
@@ -0,0 +1,41 @@
+pipelineJob('My-CI-Pipeline1') {
+    definition {
+        cps {
+            script("""
+                pipeline {
+                    agent any
+
+                    tools {
+                        maven 'mvn'
+                    }
+
+                    stages {
+                        stage('Checkout') {
+                            steps {
+                                git branch: 'demo', url: 'https://github.com/pjain287074/sample-java-maven-junit.git'
+                            }
+                        }
+
+                        stage('Compile') {
+                            steps {
+                                sh 'mvn compile -f webapp/pom.xml'
+                            }
+                        }
+
+                        stage('Test') {
+                            steps {
+                                sh 'mvn test -f webapp/pom.xml'
+                            }
+                        }
+
+                        stage('Package') {
+                            steps {
+                                sh 'mvn package -f webapp/pom.xml'
+                            }
+                        }
+                    }
+                }
+            """)
+        }
+    }
+}
---
+++ dsl_jobs/job2.groovy
@@ -0,0 +1,32 @@
+pipelineJob('My-CI-Pipeline1') {
+    definition {
+        cps {
+            script("""
+                pipeline {
+                    agent any
+
+                    tools {
+                        maven 'mvn'
+                    }
+                    stages {
+                        stage('Checkout') {
+                            steps {
+                                git branch: 'demo', url: 'https://github.com/pjain287074/sample-java-maven-junit.git'
+                            }
+                        }
+                        stage('Test') {
+                            steps {
+                                sh 'mvn test -f webapp/pom.xml'
+                            }
+                        }
+                        stage('Package') {
+                            steps {
+                                sh 'mvn package -f webapp/pom.xml'
+                            }
+                        }
+                    }
+                }
+            """)
+        }
+    }
+}
